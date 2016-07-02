a1b6fa573c53cb334bba13ccca5a3a28c739b944
Aly Naguib <aly.naguib.dev@gmail.com>
2016-07-02 08:04:32 +0200

    - Adding calls view and calls mock data. - Adding calls resources. - Restructure top views "Chats", "Calls" and "Contacts" to be in one JSON array. - Adding menu view, menu data array in top views array object.

--- scripts/app.js
+++ scripts/app.js
@@ -47,36 +47,75 @@ chattingApp.run(function ($location, $rootScope, $state, $log, $window, $documen
     $('#calls').removeClass('hidden');
     $('#contacts').removeClass('hidden');
 
-    $rootScope.currentActionImg = 'chats';
-    $rootScope.currentView = 'subviews/chats.html';
 
+    $rootScope.views = {
+        chats: {
+            mockURL: 'data/chats.json',
+            view: 'subviews/chats.html',
+            menu: ['New group', 'New broadcast', 'WhatsApp Web', 'Starred messages', 'Status', 'Settings']
+        },
+        calls: {
+            mockURL: 'data/calls.json',
+            view: 'subviews/calls.html',
+            menu: ['Clear call log', 'Status', 'Settings']
+        },
+        contacts: {
+            mockURL: 'data/contacts.json',
+            view: 'subviews/contacts.html',
+            menu: ['Refresh', 'Status', 'Settings']
+        }
+    };
+    $rootScope.currentMenu = $rootScope.views.chats.menu;
+    $rootScope.currentActionImg = 'chats';
+    $rootScope.currentView = $rootScope.views.chats.view;
 
     $rootScope.changeView = function (val) {
         $rootScope.currentActionImg = val;
         switch (val) {
             case 'chats':
-                $rootScope.currentView = 'subviews/chats.html';
+                $rootScope.currentView = $rootScope.views.chats.view;
+                $rootScope.currentMenu = $rootScope.views.chats.menu;
                 break;
             case 'calls':
-                $rootScope.currentView = 'subviews/calls.html';
+                $rootScope.currentView = $rootScope.views.calls.view;
+                $rootScope.currentMenu = $rootScope.views.calls.menu;
                 break;
             case 'contacts':
-                $rootScope.currentView = 'subviews/contacts.html';
+                $rootScope.currentView = $rootScope.views.contacts.view;
+                $rootScope.currentMenu = $rootScope.views.contacts.menu;
                 break;
             default:
-                $rootScope.currentView = 'subviews/chats.html';
+                $rootScope.currentView = $rootScope.views.chats.view;
+                $rootScope.currentMenu = $rootScope.views.chats.menu;
         }
 
     };
 
 });
 //////////////////////////////////Instead of creating each type of component in separate file///////////////////////////
+//////////////////////////////////directives////////////////////////////////////////////////////////////////////////////
+chattingApp.directive('body', function ($compile, $rootScope, $window) {
+    return {
+        restrict: 'E',
+        require: 'ngModel',
+        compile: function (element, attr, ctrl) {
+            // handel drop down menu
+            $(element).on('click', function ($event) {
+                if ($event.target != $('#icon_menu')[0]) {
+                    $('.menuContainer').removeClass('open');
+                } else {
+                    $('.menuContainer').toggleClass('open');
+                }
+            });
+        }
+    };
+});
 //////////////////////////////////services//////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////factories/////////////////////////////////////////////////////////////////////////////
 chattingApp.factory('webservices', ['$rootScope', '$log', '$http', '$q', function ($rootScope, $log, $http, $q) {
     return {
-        'get': function (url) {
+        get: function (url) {
             var deferred = $q.defer();
             $http({
                 url: url,
@@ -91,21 +130,65 @@ chattingApp.factory('webservices', ['$rootScope', '$log', '$http', '$q', functio
             }).error(function (data, status, headers, config) {
                 deferred.resolve(undefined);
             });
-            deferred.promise;
+            return deferred.promise;
+        },
+        getData: function (view) {
+            var deferred = $q.defer();
+            switch (view) {
+                case 'chats':
+                    this.get($rootScope.views.chats.mockURL).then(function (res) {
+                        deferred.resolve(res);
+                    });
+                    break;
+                case 'calls':
+                    this.get($rootScope.views.calls.mockURL).then(function (res) {
+                        for (var v = 0; v < res.length; v++) {
+                            if (res[v].dir == 0) {
+                                res[v].call_type_img = 'img/call_missed.png';
+                            } else if (res[v].dir == 1) {
+                                res[v].call_type_img = 'img/call_out.png';
+                            } else if (res[v].dir == 2) {
+                                res[v].call_type_img = 'img/call_inc.png';
+                            }
+                        }
+                        deferred.resolve(res);
+                    });
+                    break;
+                case 'contacts':
+                    this.get($rootScope.views.contacts.mockURL).then(function (res) {
+                        deferred.resolve(res);
+                    });
+                    break;
+                default:
+                    this.get($rootScope.views.chats.mockURL).then(function (res) {
+                        deferred.resolve(res);
+                    });
+            }
+            return deferred.promise;
         }
     };
 }]);
 //////////////////////////////////controllers///////////////////////////////////////////////////////////////////////////
 
+chattingApp.controller('chats', ['$rootScope', '$log', '$scope', '$location', '$state', 'webservices', function ($rootScope, $log, $scope, $location, $state, webservices) {
 
-chattingApp.controller('chats', ['$rootScope', '$log', '$scope', '$location', '$state', function ($rootScope, $log, $scope, $location, $state) {
+    webservices.getData('chats').then(function (res) {
+
+    });
 
 }]);
 
-chattingApp.controller('contacts', ['$rootScope', '$log', '$scope', '$location', '$state', function ($rootScope, $log, $scope, $location, $state) {
+chattingApp.controller('contacts', ['$rootScope', '$log', '$scope', '$location', '$state', 'webservices', function ($rootScope, $log, $scope, $location, $state, webservices) {
+    webservices.getData('contacts').then(function (res) {
+
+    });
 
 }]);
 
-chattingApp.controller('calls', ['$rootScope', '$log', '$scope', '$location', '$state', function ($rootScope, $log, $scope, $location, $state) {
+chattingApp.controller('calls', ['$rootScope', '$log', '$scope', '$location', '$state', 'webservices', function ($rootScope, $log, $scope, $location, $state, webservices) {
+
+    webservices.getData('calls').then(function (res) {
+        $scope.calls = res;
+    });
 
 }]);
